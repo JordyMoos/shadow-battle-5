53553e0b933b5e0218b65aa4ff63d5dceba9704c
Jordy Moos <jordymoos@gmail.com>
2018-11-21 21:49:44 +0100

    Adding elm websocket

---
+++ client/src/elm/Main.elm
@@ -0,0 +1,6 @@
+port module Main exposing (main)
+
+import Html
+import PortFunnels
+
+main = Html.text "Shadow Battle 5"
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 const {Elm} = require('./elm/Main.elm');
+const {PortFunnel} = require('./static/js/PortFunnel.js');
 
 try {
   let app = Elm.Main.init({
     node: document.getElementById('elm'),
     flags: {}
   });
+
+  console.log('Response from init:');
+  console.log(app);
+
+  PortFunnel.subscribe(app, {
+    modules: ['WebSocket'],
+    moduleDirectory: './static/js/PortFunnel',
+    portNames: ['cmdPort', 'subPort']
+  });
 } catch (e) {
+  console.log('Error caught:');
   console.dir(e);
-  document.getElementById('elm').innerText = 'Error: ' + e.message;
+  document.getElementsByTagName('body')[0].innerText = 'Error: ' + e.message;
 }
