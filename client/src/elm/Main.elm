a769d5ee44fbe2c72017746868be0a7fda9a65f4
Jordy Moos <jordymoos@gmail.com>
2018-11-23 13:56:53 +0100

    Added browser.application

--- client/src/elm/Main.elm
+++ client/src/elm/Main.elm
@@ -1,12 +1,16 @@
 port module Main exposing (main)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (Html)
 import Page.Home as Home
+import Url exposing (Url)
 
 
 type alias Model =
-    { pageState : PageState
+    { url : Url
+    , key : Nav.Key
+    , pageState : PageState
     }
 
 
@@ -15,7 +19,10 @@ type PageState
 
 
 type Msg
-    = HomeMsg Home.Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url
+    | HomeMsg Home.Msg
+    | NoOp
 
 
 type alias Flags =
@@ -24,24 +31,38 @@ type alias Flags =
 
 main : Program Flags Model Msg
 main =
-    Browser.element
+    Browser.application
         { init = init
         , view = view
         , update = update
         , subscriptions = subscriptions
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
         }
 
 
-init : Flags -> ( Model, Cmd Msg )
-init flags =
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
     let
         ( homeModel, homeCmd ) =
             Home.init
     in
-    ( { pageState = HomePage homeModel
+    ( { url = url
+      , key = key
+      , pageState = HomePage homeModel
       }
     , Cmd.map HomeMsg homeCmd
     )
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest urlRequest =
+    NoOp
+
+
+onUrlChange : Url -> Msg
+onUrlChange url =
+    NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -50,18 +71,35 @@ update msg model =
         ( HomeMsg pageMsg, HomePage pageModel ) ->
             ( model, Cmd.none )
 
+        ( LinkClicked urlRequest, _ ) ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        ( UrlChanged url, _ ) ->
+            ( { model
+                | url = url
+              }
+            , Cmd.none
+            )
+
+        ( _, _ ) ->
+            ( model, Cmd.none )
 
 
---        ( _, _ ) ->
---            ( model, Cmd.none )
-
-
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    case model.pageState of
-        HomePage homeModel ->
-            Home.view homeModel
-                |> Html.map HomeMsg
+    { title = "Shadow Battle"
+    , body =
+        [ case model.pageState of
+            HomePage homeModel ->
+                Home.view homeModel
+                    |> Html.map HomeMsg
+        ]
+    }
 
 
 subscriptions : Model -> Sub Msg
